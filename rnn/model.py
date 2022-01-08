547567ebe252a084190a70a10f480a902095e6b3
robinhood75 <72042118+robinhood75@users.noreply.github.com>
2022-01-08 19:45:43 +0100

    Finish implementing RNN

--- rnn/model.py
+++ rnn/model.py
@@ -1,18 +1,19 @@
-import torch
 import torch.nn as nn
 
 
 class Net(nn.Module):
-    def __init__(self, obs_size, nb_currencies, hidden_size=32, *args):
+    def __init__(self, obs_size, nb_currencies, hidden_size=64):
         super().__init__()
         self.nb_currencies = nb_currencies
         self.obs_size = obs_size
 
-        self.LSTM = nn.LSTM(input_size=(nb_currencies, obs_size), hidden_size=hidden_size)
+        self.LSTM = nn.LSTM(input_size=obs_size, hidden_size=hidden_size)
         self.ReLU = nn.ReLU()
         self.fc1 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(size_2, nb_currencies)
+        self.fc2 = nn.Linear(hidden_size, 1)
 
     def forward(self, obs):
-        raise NotImplementedError
-
+        assert obs.size() == self.obs_size, f"Expected size {self.obs_size}, got {obs.size}"
+        hidden_state = self.LSTM(obs)
+        tmp = self.ReLU(self.fc1(hidden_state))
+        return self.fc2(tmp)
